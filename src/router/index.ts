7e5bebcb387e4a16e33edf54d746545915e12d66
alex <elkikos@gmail.com>
2022-05-01 14:06:26 +0200

    a

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SongOverview from '../views/SongOverview.vue'
+import HomeView from '../views/SongOverview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: SongOverview
     },
     {
       path: '/song',
